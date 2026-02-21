7976c05ff740e412149f7b9f4c7a3542a792f38b
Vitalii Khrabchuk <vitalik_av@o2.pl>
2026-02-21 19:12:48 +0100

    hw08 cpl

---
+++ app/loading.tsx
@@ -0,0 +1,10 @@
+
+import css from './page.module.css';
+
+export default function Loading() {
+  return (
+    <main className={css.main}>
+      <p>Loading, please wait...</p>
+    </main>
+  );
+}
